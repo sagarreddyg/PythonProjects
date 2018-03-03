11de3dfca10de47a99ed7fd26c05f592274cec67
sagarreddyg <gantasahgarreddy467@gmail.com>
2018-03-03 10:25:40 +0530

    Prime is up dated

--- prime.py
+++ prime.py
@@ -1,8 +1,10 @@
+count = 0
 for i in range(2,10):
+    count = 0
     for j in range(2,i):
         if i%j == 0:
-            pass
-    else:
+            count += 1
+    if count == 0:
         print(i)
 input()
 
