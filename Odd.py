70d87b41d5e12359250e594250615a2a81699629
sagarreddyg <gantasahgarreddy467@gmail.com>
2018-03-05 11:24:32 +0530

    script for add and even

--- Odd.py
+++ Odd.py
@@ -1,6 +1,9 @@
-for i in range(10):
-   if i%2 == 0:
-    pass
-   else:
-    print(i)
+num1 = int(input("enter your range from printing even number's : "))
+num2 = int(input("enter your range To printing even number's : "))
+coun = 0
+for i in range(num1, num2+1):
+   if i % 2 != 0:
+     print(i, end=" ")
+     coun += 1
+print("\n Total number of Odd numbers between {} to {} is {}".format(num1, num2, coun))
 input()
--- even.py
+++ even.py
@@ -1,8 +1,10 @@
-num = int(input("enter your range for printing even number's : "))
+num1 = int(input("enter your range from printing even number's : "))
+num2 = int(input("enter your range To printing even number's : "))
 coun = 0
-for i in range(1, num+1):
+for i in range(num1, num2+1):
   if i % 2 == 0:
     print(i, end=",")
     coun += 1
 
-print("\nTotal number of even number between 1 to {} is {}".format(num, coun))
+print("\nTotal number of even number between {} to {} is {}".format(num1, num2, coun))
+input()
