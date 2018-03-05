5e1edc7ea86d06356c6c617950929d9431086800
sagarreddyg <gantasahgarreddy467@gmail.com>
2018-03-05 11:00:53 +0530

    added final even numbe's coad

--- even.py
+++ even.py
@@ -1,8 +1,8 @@
-num = int(input("enter your range for printing even number's : ")
+num = int(input("enter your range for printing even number's : "))
 coun = 0
 for i in range(1, num+1):
   if i % 2 == 0:
-    print(i)
-	coun += 1
-print("Total number of even number between the 1 and {} is {}".format(num, coun)
+    print(i, end=",")
+    coun += 1
 
+print("\nTotal number of even number between 1 to {} is {}".format(num, coun))
