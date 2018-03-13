f11bcdcf274d8fd97667d1ec3b88acdfb725fb97
sagarreddyg <gantasahgarreddy467@gmail.com>
2018-03-13 20:10:54 +0530

    String printing in *s formate

--- PrintingNameInStarFormate.py
+++ PrintingNameInStarFormate.py
@@ -21,7 +21,7 @@ def printname():
             for row in range(0, 7):
                 for column in range(0, 7):
                     if (((column == 1 or column == 6) and (row != 0 and row != 6) and (row != 3)) or (
-                            ((row == 0 or row == 6) or row == 3) and (column > 0 and column < 6))):
+                            ((row == 0 or row == 6) or row == 3) and (0 < column < 6))):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
@@ -32,97 +32,96 @@ def printname():
             for row in range(0, 7):
                 for column in range(0, 7):
                     if (((column == 0 or column == 7) and (row != 0 and row != 6)) or (
-                            (row == 0 or row == 6) and (column > 0 and column < 6))):
+                            (row == 0 or row == 6) and (0 < column < 6))):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
                 result_str = result_str + "\n"
             print(result_str)
         elif n[i] == "D":
-            result_str = "";
+            result_str = ""
             for row in range(0, 7):
                 for column in range(0, 7):
                     if (((column == 1 or column == 6) and (row != 0 and row != 6)) or (
-                            (row == 0 or row == 6) and (column > 0 and column < 6))):
+                            (row == 0 or row == 6) and (0 < column < 6))):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
                 result_str = result_str + "\n"
             print(result_str)
         elif n[i] == "E":
-            result_str = "";
+            result_str = ""
             for row in range(0, 7):
                 for column in range(0, 7):
-                    if (((column == 1 or column == 1)) or (
-                            ((row == 0 or row == 6) or row == 3) and (column > 1 and column < 5))):
+                    if ((column == 1) or (
+                            ((row == 0 or row == 6) or row == 3) and (0 < column < 6))):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + " "
                 result_str = result_str + "\n"
             print(result_str)
         elif n[i] == "F":
-            result_str = "";
+            result_str = ""
             for row in range(0, 7):
                 for column in range(0, 7):
-                    if (((column == 1 or column == 1)) or ((row == 0 or row == 3) and (column > 1 and column < 5))):
-                        result_str = result_str + "*"
+                    if (column == 1) or ((row == 0 or row == 3) and (0 < column < 6)):
+                        result_str = result_str + "* "
                     else:
                         result_str = result_str + " "
                 result_str = result_str + "\n"
             print(result_str)
         elif n[i] == "G":
-            result_str = "";
+            result_str = ""
             for row in range(0, 7):
                 for column in range(0, 7):
-                    if (((column == 0 or column == 5) and (column < 4 or row > 2) and (row != 0 and row != 6)) or (
-                            ((row == 6 or row == 0 or (column != 1 and row == 3))) and (column > 0 and column < 6))):
+                    if ((column == 0 or column == 5) and (column < 4 or row > 2) and (row != 0 and row != 6)) or (
+                            (row == 6 or row == 0 or (column != 1 and row == 3)) and (0 < column < 6)):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
                 result_str = result_str + "\n"
             print(result_str)
         elif n[i] == "H":
-            result_str = "";
+            result_str = ""
             for row in range(0, 7):
                 for column in range(0, 7):
-                    if (((column == 0 or column == 6)) or ((row == 3) and (column > 0 and column < 6))):
+                    if (column == 0 or column == 6) or ((row == 3) and (0 < column < 6)):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
                 result_str = result_str + "\n"
             print(result_str)
         elif n[i] == "I":
-            result_str = "";
+            result_str = ""
             for row in range(0, 7):
                 for column in range(0, 7):
                     if ((column == 3 and (row != 0 and row != 6)) or (
-                            (row == 0 or row == 6) and (column > 0 and column < 6))):
+                            (row == 0 or row == 6) and (0 < column < 6))):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
                 result_str = result_str + "\n"
             print(result_str)
         elif n[i] == "J":
-            result_str = "";
+            result_str = ""
             for row in range(0, 7):
                 for column in range(0, 7):
                     if ((column == 4 and (row != 0 and row != 6)) or ((row == 0 or row == 6 or row == 5) and (
-                            (2 <= column <= 3 or row != 6) and (column == 1 or row != 5)) and (
-                                                                              column > 0 and column < 7))):
+                            (2 <= column <= 3 or row != 6) and (column == 1 or row != 5)) and (0 < column < 7))):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
                 result_str = result_str + "\n"
             print(result_str)
         elif n[i] == "K":
-            result_str = "";
+            result_str = ""
             i, p = 0, 4
             x, q = 4, 2
             for row in range(0, 7):
                 for column in range(0, 7):
-                    if (((column == 0)) or (
+                    if ((column == 0) or (
                             (row == i and column == x) or (row == p and column == q) or (column == 3 and row == 5) or (
-                            column == 4 and row == 6) and (column > 0 and column < 5))):
+                            column == 4 and row == 6) and (0 < column < 5))):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
@@ -131,59 +130,58 @@ def printname():
                 x -= 1
             print(result_str)
         elif n[i] == "L":
-            result_str = "";
+            result_str = ""
             for row in range(0, 7):
                 for column in range(0, 7):
-                    if ((column == 0) or ((row == 6) and (column > 0 and column < 6))):
+                    if (column == 0) or ((row == 6) and (0 < column < 6)):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
                 result_str = result_str + "\n"
             print(result_str)
         elif n[i] == "M":
-            result_str = "";
+            result_str = ""
 
             for row in range(0, 7):
                 for column in range(0, 7):
-                    if (((column == 0 or column == 6)) or (
+                    if ((column == 0 or column == 6) or (
                             (row == 1 and column == 1) or (row == 2 and column == 2) or (column == 3 and row == 3) or (
-                            column == 4 and row == 2) or (column == 5 and row == 1) and (column > 0 and column < 6))):
+                            column == 4 and row == 2) or (column == 5 and row == 1) and (0 < column < 6))):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
                 result_str = result_str + "\n"
             print(result_str)
         elif n[i] == "N":
-            result_str = "";
+            result_str = ""
 
             for row in range(0, 7):
                 for column in range(0, 7):
-                    if (((column == 0 or column == 6)) or (
+                    if ((column == 0 or column == 6) or (
                             (row == 1 and column == 1) or (row == 2 and column == 2) or (column == 3 and row == 3) or (
-                            column == 4 and row == 4) or (column == 5 and row == 5) and (column > 0 and column < 6))):
+                            column == 4 and row == 4) or (column == 5 and row == 5) and (0 < column < 6))):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
                 result_str = result_str + "\n"
             print(result_str)
         elif n[i] == "O":
-            result_str = "";
+            result_str = ""
             for row in range(0, 7):
                 for column in range(0, 7):
                     if (((column == 0 or column == 6) and (row != 0 and row != 6)) or (
-                            (row == 0 or row == 6) and (column > 0 and column < 6))):
+                            (row == 0 or row == 6) and (0 < column < 6))):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
                 result_str = result_str + "\n"
-            print(result_str);
-            result_str = ""
+            print(result_str)
         elif n[i] == "P":
-            result_str = "";
+            result_str = ""
             for row in range(0, 7):
                 for column in range(0, 7):
                     if (((column == 0 or column == 5) and (column < 4 or row < 4)) or (
-                            (row == 3 or row == 0) and (column > 0 and column < 5))):
+                            (row == 3 or row == 0) and (0 < column < 5))):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
@@ -192,71 +190,64 @@ def printname():
         elif n[i] == "Q":
             for row in range(8):
                 for col in range(12):
-                    if ((col == 0 or col == 4) and (row > 0 and row < 6)) or (
-                            (row == 0 or row == 6) and (col > 0 and col < 4)) or (
+                    if ((col == 0 or col == 4) and (0 < row < 6)) or (
+                            (row == 0 or row == 6) and (0 < col < 4)) or (
                             (row == 5 and col == 1) or (row == 7 and col == 3)):
-                        print("* ", end="")
-
-                    elif ((col == 6 or col == 10) and (row > 0 and row < 6)) or (
-                            (row == 0 or row == 6) and (col > 6 and col < 10)) or (
-                            (row == 5 and col == 7) or (row == 7 and col == 9)):
                         print("* ", end="")
                     else:
                         print(end="  ")
 
                 print()
         elif n[i] == "R":
-            result_str = "";
+            result_str = ""
             for row in range(0, 7):
                 for column in range(0, 7):
                     if (((column == 1 or (column == 6 and row < 4) or (column == 3 and row == 4) or (
                             column == 4 and row == 5) or (column == 5 and row == 6)) and (row != 0 and row != 3)) or (
-                            (row == 0 or row == 3) and (column > 0 and column < 6))):
+                            (row == 0 or row == 3) and (0 < column < 6))):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
                 result_str = result_str + "\n"
             print(result_str)
         elif n[i] == "S":
-            result_str = "";
+            result_str = ""
             for row in range(0, 7):
                 for column in range(0, 7):
                     if ((((column == 0 and row < 4) or (column == 6 and row > 3)) and (row != 0 and row != 6) and (
-                            row != 3)) or (((row == 0 or row == 6) or (row == 3)) and (column > 0 and column < 6))):
+                            row != 3)) or (((row == 0 or row == 6) or (row == 3)) and (0 < column < 6))):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
                 result_str = result_str + "\n"
-            print(result_str);
-            result_str = ""
+            print(result_str)
         elif n[i] == "T":
-            result_str = "";
+            result_str = ""
             for row in range(0, 7):
                 for column in range(0, 7):
-                    if ((column == 3 and (row != 0 and row != 6)) or (row == 0 and (column > 0 and column < 6))):
+                    if (column == 3 and (row != 0 and row != 6)) or (row == 0 and (0 < column < 6)):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
                 result_str = result_str + "\n"
             print(result_str)
         elif n[i] == "U":
-            result_str = "";
+            result_str = ""
             for row in range(0, 7):
                 for column in range(0, 7):
                     if (((column == 0 or column == 6) and (row != 0 and row != 6)) or (
-                            (row == 6) and (column > 0 and column < 6))):
+                            (row == 6) and (0 < column < 6))):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
                 result_str = result_str + "\n"
             print(result_str)
         elif n[i] == "V":
-            result_str = "";
+            result_str = ""
             i, x, y = 0, 0, 12
             for row in range(0, 7):
                 for column in range(0, 14):
-                    if ((((row == i and column == x) or (column == y and (6 > row == i))) and (
-                            column >= 0 and column < 15))):
+                    if ((row == i and column == x) or (column == y and (6 > row == i))) and (0 <= column < 15):
                         result_str = result_str + "*"
                     else:
                         result_str = result_str + " "
@@ -264,17 +255,15 @@ def printname():
                 i += 1
                 x += 1
                 y -= 1
-            print(result_str);
-            result_str = ""
+            print(result_str)
         elif n[i] == "W":
-            result_str = "";
+            result_str = ""
             i, x, y = 0, 0, 12
             a, b = 12, 24
             for row in range(0, 7):
                 for column in range(0, 28):
                     if ((((row == i and column == x) or (column == y and (6 > row == i)) or (
-                            row == i and column == a) or (column == b and (6 > row == i))) and (
-                                 column >= 0 and column < 29))):
+                            row == i and column == a) or (column == b and (6 > row == i))) and (0 <= column < 29))):
                         result_str = result_str + "*"
                     else:
                         result_str = result_str + " "
@@ -284,15 +273,13 @@ def printname():
                 y -= 1
                 a += 1
                 b -= 1
-            print(result_str);
-            result_str = ""
+            print(result_str)
         elif n[i] == "X":
-            result_str = "";
+            result_str = ""
             i, x, y = 0, 0, 6
             for row in range(0, 7):
                 for column in range(0, 7):
-                    if (
-                    (((row == i and column == x) or (column == y and (row == i))) and (column >= 0 and column < 7))):
+                    if ((row == i and column == x) or (column == y and (row == i))) and (0 <= column < 7):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
@@ -300,15 +287,13 @@ def printname():
                 i += 1
                 x += 1
                 y -= 1
-            print(result_str);
-            result_str = ""
+            print(result_str)
         elif n[i] == "Y":
-            result_str = "";
+            result_str = ""
             i, x, y = 0, 0, 6
             for row in range(0, 7):
                 for column in range(0, 7):
-                    if ((((row == i and (4 > column == x)) or (column == y and (row == i))) and (
-                            column >= 0 and column < 7))):
+                    if ((row == i and (4 > column == x)) or (column == y and (row == i))) and (0 <= column < 7):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
@@ -316,23 +301,26 @@ def printname():
                 i += 1
                 x += 1
                 y -= 1
-            print(result_str);
-            result_str = ""
+            print(result_str)
         elif n[i] == "Z":
-            result_str = "";
+            result_str = ""
             a, b = 6, 0
             for row in range(0, 7):
                 for column in range(0, 7):
-                    if ((((row == 0 or row == 6) or (column == a and row == b)) and (column >= 0 and column <= 6))):
+                    if ((row == 0 or row == 6) or (column == a and row == b)) and (0 <= column <= 6):
                         result_str = result_str + "* "
                     else:
                         result_str = result_str + "  "
                 result_str = result_str + "\n"
                 a -= 1
                 b += 1
-            print(result_str);
-            result_str = ""
+            print(result_str)
 
 
-printname()
-input()
+def main():
+    printname()
+    input()
+
+
+if __name__ == "__main__":
+    main()
